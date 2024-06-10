9d50e41fb91de44fc09d753d6cdbca646d68a827
B4PT0R <bferrand.maths@gmail.com>
2024-06-10 16:19:24 +0200

    //

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="streamlit_notebook",
-    version="0.0.4",
+    version="0.0.5",
     author="Baptiste Ferrand",
     author_email="bferrand.maths@gmail.com",
     description="A notebook interface for Streamlit.",
